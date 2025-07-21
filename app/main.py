eff363286f018fd62945cbd84697c1db6c8b7f24
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2025-07-21 21:22:27 +0530

    Final Commit

--- app/main.py
+++ app/main.py
@@ -3,5 +3,11 @@ from app.routes import products, orders
 
 app = FastAPI()
 
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to HROne Backend Task"}
+
+
 app.include_router(products.router)
 app.include_router(orders.router)
